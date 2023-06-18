2580db63fbe91b577f50dad18a2597d3a222f6a1
carolineolagunju <olamidejr21@gmail.com>
2023-06-18 17:42:50 -0600

    refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,14 +3,14 @@ const request = require('request');
 
 const breedFetcher = function() {
   //requesting the API
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     //check if there is error with the url
     if (error) {
       console.log(`Here is the error that occured,`, error);
     }
 
-    if (!response) {
+    if (response.statusCode !== 200) {
       console.log(`Things did not go well`);
 
     }
@@ -18,15 +18,18 @@ const breedFetcher = function() {
     const data = JSON.parse(body);
     const dataDescription = data[0].description;
   
-    if (!data.includes(data[0])) {
-      console.log(`Breed does not exist`);
+    //checking if the data does not exist
+    if (data.length === 0) {
+      console.log(`${breedName} breed does not exist`);
     } else {
-      console.log(`${dataDescription}`);
+      console.log(`${breedName}: ${dataDescription}`);
     }
   });
 };
 
-const args = process.argv.slice(2);
+
+const breedName = process.argv.slice(2);
+
 breedFetcher();
 
 
