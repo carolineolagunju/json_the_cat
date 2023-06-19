1e701c454d9bf450ad09554b0be56a382ba2e928
carolineolagunju <olamidejr21@gmail.com>
2023-06-18 21:40:55 -0600

    implement function fetchBreedDescription and clean code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,36 +1,29 @@
 const request = require('request');
 
 
-const breedFetcher = function() {
+const fetchBreedDescription = function(breedName, callback) {
   //requesting the API
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     //check if there is error with the url
     if (error) {
-      console.log(`Here is the error that occured,`, error);
+      callback(error, null);
+      return;
     }
 
-    if (response.statusCode !== 200) {
-      console.log(`Things did not go well`);
-
-    }
     //deserializing to a well structured object
     const data = JSON.parse(body);
-    const dataDescription = data[0].description;
   
     //checking if the data does not exist
     if (data.length === 0) {
-      console.log(`${breedName} breed does not exist`);
-    } else {
-      console.log(`Description: ${dataDescription}`);
+      callback(`${breedName} breed does not exist`, null);
+      return;
     }
+
+    const dataDescription = data[0].description;
+    callback(null, dataDescription);
   });
 };
 
 
-const breedName = process.argv.slice(2);
-
-breedFetcher();
-
-
-module.exports = breedFetcher;
+module.exports = {fetchBreedDescription};
